a73fe2e7284f0ec15507781a46d76152db97a5f9
Suzclem Adriana Ochoa Casillas <15171173@itculiacan.edu.mx>
2020-06-27 10:26:22 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView
+      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>I FUCKING DID IT</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
