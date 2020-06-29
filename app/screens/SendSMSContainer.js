86d797f1c81034b9f30c65fcf95529a2b3d93a36
Suzclem Adriana Ochoa Casillas <15171173@itculiacan.edu.mx>
2020-06-29 12:12:05 -0600

    not needed packs deleted

--- app/screens/SendSMSContainer.js
+++ app/screens/SendSMSContainer.js
@@ -4,29 +4,26 @@ import SendSMS from 'react-native-sms-x';
 import {ToastAndroid} from 'react-native';
 
 function SendSMSContainer() {
-  const gpsNumber = '-';
-
+  const gpsNumber = '6672047175';
+  //6674832418
   const commands = {
     start: 'resume123456',
     stop: 'stop123456',
     alarmOn: 'arm123456',
     alarmOff: 'disarm123456',
   };
-
   const commandDescription = {
     start: 'Engine Started',
     stop: 'Engine Stopped',
     alarmOn: 'Alarm On',
     alarmOff: 'Alarm Off',
   };
-
-  // Function to send command
+  // Function to send message
   function sendCommand(keyCommand) {
     SendSMS.send(1, gpsNumber, commands[keyCommand], () => {
       ToastAndroid.show(commandDescription[keyCommand], ToastAndroid.SHORT);
     });
   }
-
   return <Scan sendCommand={sendCommand} />;
 }
 
