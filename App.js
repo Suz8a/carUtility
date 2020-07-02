5e354f5a677d55658cc5fad2a643e0eaf81cbc99
Suzclem Adriana Ochoa Casillas <15171173@itculiacan.edu.mx>
2020-07-02 13:17:27 -0600

    Bottom bar added & working without icons

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import SendSMSContainer from './app/screens/SendSMSContainer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from './app/screens/Home';
+import SettingsScreen from './app/screens/SettingsScreen';
+const Tab = createBottomTabNavigator();
 
 function App() {
-  return <SendSMSContainer />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
